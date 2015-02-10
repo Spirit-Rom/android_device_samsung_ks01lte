43557d7a781556e15a9556120bd035a9a90db236
Nico60 <ndhouailly@gmail.com>
2015-02-10 15:01:46 +0100

    Spirit Stuff

---
+++ spirit.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/ks01lte/full_ks01lte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/spirit/config/nfc_enhanced.mk)
+
+# Inherit some common SR stuff.
+$(call inherit-product, vendor/spirit/config/common_phone.mk)
+
+PRODUCT_DEVICE := ks01lte
+PRODUCT_NAME := spirit_ks01lte
